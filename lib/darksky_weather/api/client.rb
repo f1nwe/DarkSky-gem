49155145d9ddda07f3b7a15195c8e8069113e8ae
Volodymyr Kaban <kaban.volodymyr@gmail.com>
2019-11-07 20:31:07 +0300

    allow to pass http options to client

--- lib/darksky_weather/api/client.rb
+++ lib/darksky_weather/api/client.rb
@@ -6,16 +6,23 @@ module DarkskyWeather
       include HTTParty
       base_uri "https://api.darksky.net/forecast/"
 
-      def get_weather(lat:, lng:, timestamp: nil)
+      def get_weather(lat:, lng:, timestamp: nil, **options)
         key         = DarkskyWeather::Api.configuration.api_key
 
         request_path   = "/#{key}/#{lat},#{lng}"
         request_path << ",#{timestamp}" if timestamp
+        request_path << prepare_options(options) if options.any?
 
         raw_result  = self.class.get(request_path)
         request_uri = raw_result.request.uri.to_s
 
         return WeatherData.new(timestamp, request_uri, raw_result.parsed_response)
+      end
+
+      private
+
+      def prepare_options(hash_options)
+        '?' + hash_options.map { |k, v| "#{k}=#{v}" }.join('&')
       end
     end
   end
